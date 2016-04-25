0289f25d5fb3af9fff7fd9298d58e7760ddb48ba
TanZhiWei <tanzhiwei0102@163.com>
2016-04-25 08:36:13 +0800

    第一次创建项目

---
+++ HappyBuy/HappyBuy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HappyBuy
+//
+//  Created by TanZhiWei on 16/4/25.
+//  Copyright © 2016年 TanZhiWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HappyBuy/HappyBuy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HappyBuy
+//
+//  Created by TanZhiWei on 16/4/25.
+//  Copyright © 2016年 TanZhiWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
